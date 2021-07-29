f729fb8a903ff764bd3a6dbde11463605e853b0c
TIRATH PATHAK <tirathp.wot2021@gmail.com>
2021-07-29 14:32:41 +0530

    Added

---
+++ training_session1/html/html.js
@@ -0,0 +1,11 @@
+function myFun(){
+    var name = ['Tirath','Ankit','Anuj','Sachin'];
+    for(i=0 ;i<name.length;i++){
+        console.log(name[i]);
+        // document.write(name[i]);
+    }
+}
+
+myFun();
+
+ 
